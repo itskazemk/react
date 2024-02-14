5c9e2e44158984f1ba5006d286f4dd864d5fecfe
Sanpad User1 <sanpad_user1@hotmail.com>
2024-02-14 16:54:03 +0330

    steps done

---
+++ 03-steps/src/App.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((step) => step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((step) => step + 1);
+    }
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <div className="message">
+            <p>
+              step {step}: {messages[step - 1]}
+            </p>
+          </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => {
+                handlePrevious();
+              }}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- 03-steps/src/index.js
+++ 03-steps/src/index.js
@@ -9,5 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-//dummy comment
