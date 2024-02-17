e4ddb679c90bc1bd65372ec882e2b01f5fa1ef10
Sanpad User1 <sanpad_user1@hotmail.com>
2024-02-17 11:08:31 +0330

    counter project styling

--- 04-counter/src/index.js
+++ 04-counter/src/index.js
@@ -1,34 +1,6 @@
-import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
-  // const [date, setDate] = useState(new Date().toDateString());
-
-  // if (step > 0) setCount((count) => count * step);
-
-  const date = new Date("june 21 2027");
-  date.setDate(date.getDate() + count);
-
-  return (
-    <div>
-      <div>
-        <button onClick={() => setStep((val) => val - 1)}>-</button>Step: {step}
-        <button onClick={() => setStep((val) => val + 1)}>+</button>
-      </div>
-      <div>
-        <button onClick={() => setCount((val) => val - step)}>-</button>Count:{" "}
-        {count}
-        <button onClick={() => setCount((val) => val + step)}>+</button>
-      </div>
-      <p>
-        {count} days from now is {date.toDateString()}
-      </p>
-    </div>
-  );
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
