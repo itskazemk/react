e118a8a147b9bb9f1ebecfb9f2ba2e4ecfd4007c
Sanpad User1 <sanpad_user1@hotmail.com>
2024-02-17 11:36:52 +0330

    counter-forked done

--- 04-counter-fork/src/App.js
+++ 04-counter-fork/src/App.js
@@ -11,20 +11,43 @@ export default function App() {
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
   return (
     <div className="app">
       <div>
-        <button onClick={() => setStep((val) => val - 1)}>-</button>Step: {step}
-        <button onClick={() => setStep((val) => val + 1)}>+</button>
+        {/* <button onClick={() => setStep((val) => val - 1)}>-</button>Step: {step} */}
+        {/* <button onClick={() => setStep((val) => val + 1)}>+</button> */}
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
       </div>
       <div>
-        <button onClick={() => setCount((val) => val - step)}>-</button>Count:{" "}
-        {count}
-        <button onClick={() => setCount((val) => val + step)}>+</button>
+        <button onClick={() => setCount((val) => val - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
       </div>
+      <button onClick={() => setCount((val) => val + step)}>+</button>
       <p>
-        {count} days from now is {date.toDateString()}
+        {count === 0 ? null : `${count}days from`} Today is{" "}
+        {date.toDateString()}
       </p>
+      {count !== 0 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
