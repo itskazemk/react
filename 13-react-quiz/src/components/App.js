222ce97a4cd4925ea43db148ee7d11fc11894848
Sanpad User1 <sanpad_user1@hotmail.com>
2024-03-06 13:52:17 +0330

    010 Handling New Answers

--- 13-react-quiz/src/components/App.js
+++ 13-react-quiz/src/components/App.js
@@ -13,6 +13,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -26,7 +27,15 @@ function reducer(state, action) {
       return { ...state, status: "active" };
 
     case "newAnswer":
-      return { ...state, answer: action.payload };
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Error in reducer");
   }
