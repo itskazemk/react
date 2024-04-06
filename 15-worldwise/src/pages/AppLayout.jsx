45d57ffa2a33344379b2c2c433a77911c77e5e03
Sanpad User1 <sanpad_user1@hotmail.com>
2024-04-06 10:30:32 +0330

    added biome

--- 15-worldwise/src/pages/AppLayout.jsx
+++ 15-worldwise/src/pages/AppLayout.jsx
@@ -1,17 +1,19 @@
-import Map from "../components/Map";
-import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import User from "../components/User.jsx";
 import { memo } from "react";
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
 
 const AppLayout = memo(function AppLayout() {
-  return (
-    <div className={styles.app}>
-      <Sidebar />
-      <Map />
-      <User />
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<Sidebar />
+
+			<Map />
+
+			<User />
+		</div>
+	);
 });
 
 export default AppLayout;
