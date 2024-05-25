1c806024df0e6e91633883544a7c3fc79d13c653
Kazem Kamali <itskazemk@gmail.com>
2024-05-25 19:45:44 +0330

    009 Handling Errors With Error Elements.mp4

--- 19-fast-react-pizza/src/ui/Error.jsx
+++ 19-fast-react-pizza/src/ui/Error.jsx
@@ -1,15 +1,19 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from "react-router-dom";
 
-function NotFound() {
+function Error() {
   const navigate = useNavigate();
+  const error = useRouteError();
+  console.log(1111111111, error);
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <h2>Status Code: {error.status}</h2>
+      <h3>Status Text: {error.statusText}</h3>
+      <h4 style={{ background: "#ff7b7b" }}>{error.data ?? error.message}</h4>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
 }
 
-export default NotFound;
+export default Error;
