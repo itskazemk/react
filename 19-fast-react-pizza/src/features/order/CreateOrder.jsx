16ed0a8ca51944f982c1e929ed3a72a4c28b40fd
Kazem Kamali <itskazemk@gmail.com>
2024-05-26 12:09:21 +0330

    ccc

--- 19-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 19-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -71,6 +71,7 @@ function CreateOrder() {
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button>Order now</button>
         </div>
       </Form>
@@ -81,7 +82,15 @@ function CreateOrder() {
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+
   console.log("formData", data);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+  console.log("order", order);
+
   return null;
 }
 
